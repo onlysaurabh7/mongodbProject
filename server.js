a8f83e25d16556fcea226d58bb743227e8ddce99
Saurabh Tiwari <onlysaurabh706@gmail.com>
2021-10-05 02:39:49 -0400

    index.html and server.js updated again

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const emailValidator = require('email-validator');
+emailValidator.validate("test@email.com"); // true
 const bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -11,33 +12,76 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Get Request Route
 app.get('/', function (req, res) {
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+        if (err) {
+            console.log(err);
+        } else if (db) {
+            var dbo = db.db("mydatabase"); // creating Database
+            dbo.collection("mycollection").find({}).toArray(function (err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                console.log(res)
+                db.close();
+            });
+        }
+    })
     res.sendFile(__dirname + '/index.html')
 });
 
 
 // Post Request Route
-app.post('/insert', function(req, res){
+app.post('/', function (req, res) {
     // console.log(req.data);
-    console.log(req.body);
-    let myobj = req.body
-    console.log(myobj);
-    MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db){
-        if (err){
-            console.log(err);
-        } else if(db) {
-            var dbo = db.db(""); // creating Database
-            dbo.collection("").insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-            });
-        }
-    })
-    
-    res.sendFile(__dirname+'/index.html')
+    // var firstName = req.body.firstName;
+    // var lastName = req.body.lastName;
+    var email = req.body.email;
+    // var address = req.body.address;
+    // var mobile = req.body.mobile;
+    // var date = req.body.date;
+    // let object = [{
+    //     firstName : "req.body.firstName",
+    //     lastName : "req.body.lastName",
+    //     email : "req.body.email",
+    //     address :"req.body.address",
+    //     mobile :"req.body.mobile",
+    //     date : "req.body.date"
+    // }]
+
+    if (emailValidator.validate(email)) {
+        // console.log(req.body);
+        let myobj = req.body;
+        // console.log(myobj);
+        MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+            if (err) {
+                console.log(err);
+            } else if (db) {
+                var dbo = db.db("mydatabase"); // creating Database
+                dbo.collection("mycollection").findOne({ email: req.body.email }, function (err, res) {
+                    // console.log("data", res)
+                    if (res) {
+                        console.log("Email Already Exist");
+                    } else {
+                        dbo.collection("mycollection").insertOne(myobj, function (err, res) {
+                            if (err) throw err;
+                            console.log("1 document inserted");
+                            // res.render('myobj');
+                            db.close();
+                        });
+                    }
+                });
+
+            }
+        })
+    }
+    else {
+        res.send("Email is Invalid");
+    }
+    res.sendFile(__dirname + '/index.html')
+
 });
 
 // Server Port
-app.listen(3000, function () {
-    console.log('server is listening on port ${port:3000}');
-})
+app.listen(5000, function () {
+    console.log('server is listening on port ${port:5000}');
+});
+
